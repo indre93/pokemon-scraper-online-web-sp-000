495434d2e137792099be524005df3b29ff08e07e
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 02:03:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,6 +11,7 @@ class Pokemon
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)")
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")
   end
 
   def self.find
