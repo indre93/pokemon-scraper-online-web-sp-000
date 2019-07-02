56c01bfd25a883ae9de860f3f01b2d795cf2693a
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 01:38:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,11 @@ class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
 
-  def initialize(id = nil, name = nil, type = nil)
+  def initialize(id = nil, name = nil, type = nil, db)
     @id = id
     @name = name
     @type = type
+    @db = db
   end
 
   def save
