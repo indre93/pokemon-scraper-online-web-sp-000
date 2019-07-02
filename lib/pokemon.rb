548cba177481978695950f416ac8cc52dd16692b
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 01:56:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,12 +10,9 @@ class Pokemon
   end
 
   def self.save(name, type, db)
-    sql = <<-SQL
-      INSERT INTO pokemon (name, type)
-      VALUES (?,?)
-    SQL
+    sql = INSERT INTO pokemon (name, type) VALUES (?,?)
     db.execute(sql, self.name, self.type)
-    @id = DB[SQLite3::Database.new].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
   def self.find
